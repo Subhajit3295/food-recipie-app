ae4e3a950c1449e8b90f6088192c6c626451ace5
Subhajit3295 <clawdrax@gmail.com>
2025-02-11 15:23:53 +0530

    bugs fixed

--- food-recipe/src/Recipes/recipecard.jsx
+++ food-recipe/src/Recipes/recipecard.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const RecipeCard = (props) => {
   return (
-    <div className="max-w-sm rounded-2xl h-auto shadow-lg bg-white active:bg-orange-400 active:text-white duration-100 hover:text-white hover:bg-orange-400 focus:bg-orange-500 p-4" key={props.key}>
+    <div className="max-w-sm rounded-2xl h-auto shadow-lg bg-white achover:bg-amber-400 focus:bg-amber-400 hover:bg-amber-400 hover:cursor-pointer hover:scale-103 p-4 active:bg-amber-400 duration-300" key={props.key}>
       <img className="w-full h-48 object-cover rounded-xl" src={props.image} alt={props.mealName} />
       <div className="py-4">
         <h2 className="text-xl font-bold text-gray-800">{props.mealName}</h2>
